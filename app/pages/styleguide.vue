5fb183f3cbbb79b41580435b32df1562034f1ab6
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-06-17 18:55:30 -0300

    Add /styleguide page shell with NuxtPage routing

---
+++ app/pages/styleguide.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-canvas-soft px-6 py-10">
+    <h1 class="mb-8 font-display font-black text-[32px] text-ink">
+      Styleguide
+    </h1>
+  </div>
+</template>
